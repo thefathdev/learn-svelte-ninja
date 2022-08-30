46c39485c1daf2da58b22b5bf5173a7735377cf1
Fathul Irfaan Abdillah <abdillahfathu@gmail.com>
2022-08-30 10:27:47 +0700

    Added Header components

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,18 @@
 <script>
+  import Header from './lib/Header.svelte'
 </script>
 
-<main>
-  <h1>Hello World</h1>
-</main>
+<Header />
+<main />
 
 <style>
+  :global(:root) {
+    --clr-primary: hsl(37, 95%, 56%);
+    --clr-secondary: hsl(0, 0%, 25%);
+  }
+
+  main {
+    max-width: 60rem;
+    margin-inline: auto;
+  }
 </style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,29 @@
+<header>
+  <h1>Orange<span>Poll</span></h1>
+  <img width="256" src="/img/undraw_charts.svg" alt="Chart poll" />
+</header>
+
+<style>
+  header {
+    max-width: 60rem;
+    margin: 2em auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5%;
+  }
+
+  h1 {
+    font-family: sans-serif;
+    display: flex;
+    flex-direction: column;
+    color: var(--clr-primary);
+    font-weight: bold;
+  }
+
+  h1 span {
+    color: var(--clr-secondary);
+    text-decoration: underline;
+    font-weight: 400;
+  }
+</style>
